5fd3f9c800538c57a5d19256ed6e5502fb1f00e8
Ali Haydar Güleç <alihaydargulec@gmail.com>
2022-10-23 15:09:17 +0300

    UI Changes, Selectable Results

--- fonks.js
+++ fonks.js
@@ -1,25 +1,40 @@
-function predict(
-  prompt,
-  sampler = "Euler a",
-  steps = 3,
-  seed = -1,
-  w = 512,
-  h = 512,
-  tiling = false
-) {
+var loadingAnim;
+var angularSpeed = 8;
+var latestPrompt = "";
+loadingAnim = new Konva.Animation(function (frame) {
+  let currentSWidth = controller.strokeWidth();
+  var angleDiff = angularSpeed * (frame.timeDiff / 1000);
+
+  if (currentSWidth >= 7) {
+    angularSpeed = -8;
+  } else if (currentSWidth <= 2) {
+    angularSpeed = 8;
+  }
+  controller.strokeWidth(currentSWidth + angleDiff);
+}, layer);
+
+function predict(options) {
+  changeDraggable(false);
+  $("#save").slideDown("slow");
+  let prompt = options.prompt || "fatih sultan mehmet",
+    sampler = options.sampler || "Euler a",
+    steps = options.steps || 10,
+    seed = options.seed || -1,
+    w = options.w || 512,
+    h = options.h || 512,
+    tiling = options.tiling || false;
+  latestPrompt = prompt;
   $.ajax({
-    url: "http://127.0.0.1:7860/v1/txt2img",
+    url: "http://127.0.0.1:3000/txt2img",
     type: "POST",
     data: JSON.stringify({
-      txt2imgreq: {
-        prompt,
-        steps,
-        sampler_name: sampler,
-        seed,
-        width: w,
-        height: h,
-        tiling,
-      },
+      prompt,
+      steps,
+      sampler_name: sampler,
+      seed,
+      width: w,
+      height: h,
+      tiling,
     }),
     contentType: "application/json",
     complete: (res) => {
@@ -27,22 +42,142 @@ function predict(
         placeY = controller.attrs.y;
 
       let result = res.responseJSON;
-      let firstimage = result.images[0];
+      let firstimage = result.image;
       var imageObj1 = new Image();
       imageObj1.src = "data:image/png;base64," + firstimage;
       // darth vader
-      let darthVaderImg = new Konva.Image({
+      let newImage = new Konva.Image({
         width: 512,
         height: 512,
         x: placeX,
         y: placeY,
         image: imageObj1,
       });
-      darthVaderImg.off("click");
 
-      layer.add(darthVaderImg);
+      newImage.off("click");
+
+      layer.add(newImage);
       let zIndex = controller.zIndex();
-      controller.zIndex(zIndex + 1);
+      controller.zIndex(zIndex + 2);
+      changeDraggable(true);
     },
   });
 }
+var outPaintImgs = [];
+var currIdx = 0;
+function outpaint(options) {
+  changeDraggable(false);
+  let prompt = options.prompt || "fatih sultan mehmet",
+    image = options.image;
+  latestPrompt = prompt;
+  $.ajax({
+    url: "http://127.0.0.1:3000/outpaint",
+    type: "POST",
+    data: JSON.stringify({
+      prompt,
+      image,
+    }),
+    contentType: "application/json",
+    complete: (res) => {
+      let result = res.responseJSON;
+      result.images.shift();
+      outPaintImgs = [];
+      currIdx = 0;
+      result.images.forEach((image) => {
+        let imObj = new Image();
+        imObj.src = "data:image/png;base64," + image;
+        outPaintImgs.push(imObj);
+      });
+
+      controller.fillPatternImage(outPaintImgs[currIdx]);
+      loadingAnim.stop();
+      $("#selector").slideDown("slow");
+      $("#currentIndex").text(currIdx + 1);
+      //layer.add(newImage);
+    },
+  });
+}
+function changeDraggable(can) {
+  if (!can) {
+    loadingAnim.start();
+  } else {
+    controller.strokeWidth(1);
+    loadingAnim.stop();
+  }
+  controller.draggable(can);
+}
+
+function loadingSequence() {
+  for (let st = 0; st < 20; st += 0.05) {
+    console.log(st);
+    setTimeout(() => {
+      controller.strokeWidth(21 - st);
+    }, st * 230);
+  }
+}
+
+$("#previousImg").click(() => {
+  if (currIdx > 0) {
+    currIdx--;
+  }
+  $("#currentIndex").text(currIdx + 1);
+  controller.fillPatternImage(outPaintImgs[currIdx]);
+});
+$("#nextImg").click(() => {
+  if (currIdx < outPaintImgs.length - 1) {
+    currIdx++;
+  }
+  $("#currentIndex").text(currIdx + 1);
+  controller.fillPatternImage(outPaintImgs[currIdx]);
+});
+$("#btnAccept").click(() => {
+  let placeX = controller.attrs.x,
+    placeY = controller.attrs.y;
+
+  let newImage = new Konva.Image({
+    width: 512,
+    height: 512,
+    x: placeX,
+    y: placeY,
+    image: outPaintImgs[currIdx],
+  });
+
+  controller.fillPatternImage(null);
+  changeDraggable(true);
+  layer.add(newImage);
+  let zIndex = controller.zIndex();
+  controller.zIndex(zIndex + 1);
+  $("#selector").slideUp("slow");
+});
+$("#btnCancel").click(() => {
+  controller.fillPatternImage(null);
+  changeDraggable(true);
+  $("#selector").slideUp("slow");
+});
+
+$("#save").click(function () {
+  let oldScale = stage.scale();
+  stage.scale({ x: 1, y: 1 });
+
+  controller.visible(false);
+  bgLayer.visible(false);
+  bdImage.visible(false);
+  var image = stage.toImage({
+    x: minX,
+    y: minY,
+    width: maxX - minX,
+    height: maxY - minY,
+    pixelRatio: 1,
+    callback(img) {
+      let base64 = img.src.split(",")[1];
+      var download = document.createElement("a");
+      download.href = img.src;
+      download.download = latestPrompt + ".png";
+      download.click();
+    },
+  });
+  controller.visible(true);
+  bgLayer.visible(true);
+  bdImage.visible(true);
+  stage.scale(oldScale);
+});
--- stage.js
+++ stage.js
@@ -1,18 +1,78 @@
 // first we need to create a stage
 var width = window.innerWidth;
 var height = window.innerHeight;
+var toolbarmode = "select";
+var sceneWidth = 1920;
+var sceneHeight = 1080;
+$(".toolbar").click(function () {
+  $(".toolbar").removeClass("btn-primary").addClass("btn-secondary");
+  $(this).removeClass("btn-secondary").addClass("btn-primary");
+
+  let mode = $(this).attr("role");
+  toolbarmode = mode;
+  if (mode == "pan") {
+    stage.container().style.cursor = "grabbing";
+    stage.draggable(true);
+    controller.draggable(false);
+  }
+  if (mode == "select") {
+    stage.draggable(false);
+    controller.draggable(true);
+  }
+  if (mode == "erase") {
+    stage.draggable(false);
+    controller.draggable(false);
+  }
+  controller.zIndex(515);
+});
 var stage = new Konva.Stage({
   container: "container", // id of container <div>
-  width: width,
-  height: height,
+  width: sceneWidth,
+  height: sceneHeight,
   background: "#ffda44",
   stroke: "black",
   strokeWidth: 1,
-  draggable: true,
+  draggable: false,
 });
-var scaleBy = 1.11;
+$("#btnZoomOut").click(() => {
+  stageZoom(0.8);
+  $("#scaleHelpText").text(Math.round(stage.scale().x * 100) + "%");
+});
+$("#btnZoomIn").click(() => {
+  stageZoom(1.2);
+  $("#scaleHelpText").text(Math.round(stage.scale().x * 100) + "%");
+});
+$("#scaleHelpText").click(() => {
+  let current = stage.scaleX();
+  let target = 1 / current;
+  stageZoom(target);
+  $("#scaleHelpText").text(Math.round(stage.scale().x * 100) + "%");
+});
+$("#scaleHelpText").text(stage.scale().x * 100 + "%");
+var scaleBy = 2;
 
+function fitStageIntoParentContainer() {
+  var container = document.querySelector("#stage-parent");
+
+  // now we need to fit stage into parent container
+  var containerWidth = container.offsetWidth;
+
+  // but we also make the full scene visible
+  // so we need to scale all objects on canvas
+  var scale = containerWidth / sceneWidth;
+
+  stage.width(sceneWidth * scale);
+  stage.height(sceneHeight * scale);
+  stage.scale({ x: scale, y: scale });
+}
+
+fitStageIntoParentContainer();
+// adapt the stage on any window resize
+window.addEventListener("resize", fitStageIntoParentContainer);
 stage.on("wheel", (e) => {
+  let oldpos = stage.position();
+  stage.position({ x: oldpos.x, y: oldpos.y - e.evt.deltaY / 5 });
+  return;
   // stop default scrolling
   e.evt.preventDefault();
 
@@ -48,23 +108,191 @@ stage.on("wheel", (e) => {
 var layer = new Konva.Layer();
 // create our shape
 var controller = new Konva.Rect({
-  x: 20,
-  y: 20,
+  x: width / 2 - 256,
+  y: height / 2 - 256,
   width: 512,
   height: 512,
-  fill: "#00ffff10",
-  stroke: "blue",
-  strokeWidth: 0.4,
-  dash: [3, 3],
+  fill: "#00ffff00",
+  stroke: "#2e09ff",
+  strokeWidth: 1,
+  // dash: [13, 20],
   draggable: true,
 });
+controller.fillPriority("pattern");
+
 // add the shape to the layer
 layer.add(controller);
+controller.position({
+  x: Math.round(controller.x() / 22.2222222) * 22.2222222,
+  y: Math.round(controller.y() / 22.2222222) * 22.2222222,
+});
 
 // darth vader
+var backDrop = new Image();
+let bdImage = new Konva.Image({
+  width: 512,
+  height: 512,
+  x: width / 2 - 256,
+  y: height / 2 - 256,
+  image: backDrop,
+});
 
+backDrop.src = "./assets/backdrop.png";
+backDrop.onload = () => {
+  layer.add(bdImage);
+  bdImage.zIndex(0);
+  bdImage.position(controller.position());
+};
+
+let helptext1 = new Konva.Text({
+  text: "Diffusion Frame",
+  fontSize: 18,
+  fontFamily: "ColfaxAI",
+  fill: "#2e09ff",
+  width: 512,
+
+  align: "left",
+});
+layer.add(helptext1);
+helptext1.position({
+  x: controller.position().x,
+  y: controller.position().y - 18,
+});
+controller.on("dragmove", (e) => {
+  controller.position({
+    x: Math.round(controller.x() / 22.2222222) * 22.2222222,
+    y: Math.round(controller.y() / 22.2222222) * 22.2222222,
+  });
+  bdImage.position(controller.position());
+  helptext1.position({
+    x: controller.position().x,
+    y: controller.position().y - 20,
+  });
+  stage.batchDraw();
+});
+controller.on("mouseenter", function () {
+  stage.container().style.cursor = "pointer";
+});
+
+controller.on("mouseleave", function () {
+  stage.container().style.cursor = "default";
+});
+controller.on("mousedown", function () {
+  stage.container().style.cursor = "grabbing";
+});
+controller.on("mouseup", function () {
+  stage.container().style.cursor = "pointer";
+});
 // add the layer to the stage
 stage.add(layer);
 // draw the image
 layer.draw();
 Konva.hitOnDragEnabled = true;
+
+const bgLayer = new Konva.Layer();
+stage.add(bgLayer);
+const WIDTH = 400;
+const HEIGHT = 400;
+
+function checkShapes() {
+  const startX = Math.floor((-stage.x() - stage.width()) / WIDTH) * WIDTH;
+  const endX = Math.floor((-stage.x() + stage.width() * 2) / WIDTH) * WIDTH;
+
+  const startY = Math.floor((-stage.y() - stage.height()) / HEIGHT) * HEIGHT;
+  const endY = Math.floor((-stage.y() + stage.height() * 2) / HEIGHT) * HEIGHT;
+  var imageObj = new Image();
+
+  for (var x = startX; x < endX; x += WIDTH) {
+    for (var y = startY; y < endY; y += HEIGHT) {
+      var yoda = new Konva.Image({
+        x,
+        y,
+        image: imageObj,
+        zIndex: 0,
+      });
+      // add the shape to the layer
+      bgLayer.add(yoda);
+      imageObj.src = "./assets/dots.webp";
+    }
+  }
+}
+
+checkShapes();
+bgLayer.draw();
+bgLayer.zIndex(0);
+
+stage.on("dragend", () => {
+  bgLayer.destroyChildren();
+  checkShapes();
+  bgLayer.draw();
+});
+
+function stageZoom(scaleBy) {
+  var oldScale = stage.scaleX();
+
+  var center = {
+    x: stage.width() / 2,
+    y: stage.height() / 2,
+  };
+
+  var relatedTo = {
+    x: (center.x - stage.x()) / oldScale,
+    y: (center.y - stage.y()) / oldScale,
+  };
+
+  var newScale = oldScale * scaleBy;
+
+  stage.scale({
+    x: newScale,
+    y: newScale,
+  });
+
+  var newPos = {
+    x: center.x - relatedTo.x * newScale,
+    y: center.y - relatedTo.y * newScale,
+  };
+
+  stage.position(newPos);
+  stage.batchDraw();
+}
+var isPaint = false;
+
+var lastLine;
+
+stage.on("mousedown touchstart", function (e) {
+  isPaint = true;
+  var pos = stage.getPointerPosition();
+  if (toolbarmode == "erase") {
+    lastLine = new Konva.Line({
+      stroke: "#df4b26",
+      strokeWidth: 55,
+      globalCompositeOperation:
+        toolbarmode === "erase" ? "destination-out" : "source-over",
+      // round cap for smoother lines
+      lineCap: "round",
+      lineJoin: "round",
+      // add point twice, so we have some drawings even on a simple click
+      points: [pos.x, pos.y, pos.x, pos.y],
+    });
+    layer.add(lastLine);
+  }
+});
+
+stage.on("mouseup touchend", function () {
+  isPaint = false;
+});
+
+// and core function - drawing
+stage.on("mousemove touchmove", function (e) {
+  if (!isPaint) {
+    return;
+  }
+
+  // prevent scrolling on touch devices
+  e.evt.preventDefault();
+  if (toolbarmode == "erase") {
+    const pos = stage.getPointerPosition();
+    var newPoints = lastLine.points().concat([pos.x, pos.y]);
+    lastLine.points(newPoints);
+  }
+});
